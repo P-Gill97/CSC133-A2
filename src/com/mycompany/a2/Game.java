a80f7133bf68cb690867a7432b26560b9ee6ed51
P-Gill97 <pgill@csus.edu>
2019-04-08 23:34:45 -0700

    added UI code

--- src/com/mycompany/a2/Game.java
+++ src/com/mycompany/a2/Game.java
@@ -5,13 +5,18 @@ import com.codename1.ui.Label;
 import com.codename1.ui.TextField;
 import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.events.ActionListener;
+import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.layouts.FlowLayout;
+import com.codename1.ui.plaf.Border;
 import com.mycompany.a2.Commands.*;
+import com.codename1.charts.util.ColorUtil;
 import com.codename1.ui.*;
 
 
 public class Game extends Form {
 
-	private GameWorld gameworld;
+	private GameWorld world;
 	private MapView mapView;
 	private PointsView pointsView; 
 	
@@ -40,10 +45,70 @@ public class Game extends Form {
 	private TurnPSLeftC turnPSLeftCommand; 
 	private TurnPSRIghtC turnPSRightCommand;
 	
+	@SuppressWarnings("deprecation")
 	public Game() {
 	
-		gameworld = new GameWorld();
-		gameworld.init(4,4);
+		world = new GameWorld();
+		setLayout(new BorderLayout());
+		mapView = new MapView();
+		pointsView = new PointsView(world);
+		
+		
+		Toolbar toolbar = new Toolbar(); 
+		this.setToolBar(toolbar);
+		toolbar.setTitle("ROCKS IN SPACE");
+		
+		// north
+		Container northContainer = new Container(); 
+		
+		northContainer.setLayout(new BoxLayout(BoxLayout.Y_AXIS));
+		
+		
+		// middle
+		Container middleContainer = new Container(); 
+		middleContainer.setLayout(new FlowLayout(Component.CENTER));
+		
+		Container southContainer = new Container(); 
+		southContainer.setLayout(new BoxLayout(BoxLayout.X_AXIS));
+		southContainer.add(pointsView); 
+		
+		northContainer.add(middleContainer);
+		northContainer.add(southContainer);
+		
+		
+		Container buttonContainer = new Container(); 
+		buttonContainer.setLayout(new BoxLayout(BoxLayout.Y_AXIS));
+		buttonContainer.add(new Label("Commands: "));
+		buttonContainer.getAllStyles().setBgColor(ColorUtil.WHITE);
+		buttonContainer.getAllStyles().setBorder(Border.createLineBorder(5, ColorUtil.CYAN ));
+		buttonContainer.setScrollableY(true);
+		
+		// asteroid
+		addAsteroidCommand = new AddAsteroidC(world); 
+		Button addAsteroidButton = new Button("Add Asteroid");
+		addAsteroidButton.getAllStyles().setBgTransparency(1);
+		addAsteroidButton.getAllStyles().setFgColor(ColorUtil.WHITE);
+		addAsteroidButton.getAllStyles().setBorder(Border.createLineBorder(2,ColorUtil.CYAN));
+		addAsteroidButton.setCommand(addAsteroidCommand);
+		buttonContainer.add(addAsteroidButton);
+		// add NPS 
+		
+		
+		
+		
+		
+		
+		
+		
+		// -------------------
+		
+		
+		world.addObserver(mapView);
+		world.addObserver(pointsView);
+		
+		add(BorderLayout.NORTH,northContainer);
+		this.show();
+		world.init(mapView.getWidth(), mapView.getHeight());
 		
 		
 	}
--- src/com/mycompany/a2/Starter.java
+++ src/com/mycompany/a2/Starter.java
@@ -51,7 +51,7 @@ public class Starter {
             current.show();
             return;
         }
-        
+        new Game();
     }
 
     public void stop() {
